d759b9a5e2c6b451f99a92c3e80f6be93d967ec0
kn4y4n <kn4y4nnn@gmail.com>
2023-05-06 21:41:40 +0530

    feat: archive utils

--- archive/models.py
+++ archive/models.py
@@ -1,21 +1,5 @@
+from operator import index
 from django.db import models
-from datetime import datetime
-from uuid import uuid4
-
-
-def get_event_id(instance):
-    """
-    Generates a unique identifier for naming the screenshot
-    """
-    event_id = instance.created_at.strftime("%Y-%m-%d-%H-%M-%S-") + str(uuid4())
-    return event_id
-
-
-def path_generator(instance):
-    """
-    Uses event_id and resource_id to generate a unique path
-    """
-    return f"archive/{instance.resource.id}/{instance.get_event_id()}/"
 
 
 class Resource(models.Model):
@@ -34,12 +18,28 @@ class Archive(models.Model):
     Model for representing a archive, could be a HTML file, Image file etc.
     """
 
+    STATUS_CHOICES = (
+        ("pending", "pending"),
+        ("enqueued", "enqueued"),
+        ("complete", "complete"),
+        ("failed", "failed"),
+    )
+
     resource = models.ForeignKey(
         Resource, related_name="archives", on_delete=models.CASCADE
     )
     created_at = models.DateTimeField(auto_now_add=True)
     source = models.ImageField(
-        upload_to=path_generator, help_text="store the image of the resource"
+        upload_to=f"uploads/archive/",
+        help_text="store the image of the resource",
+        null=True,
+        blank=True,
+    )
+    status = models.CharField(
+        max_length=255,
+        choices=STATUS_CHOICES,
+        default="pending",
+        db_index=True,
     )
 
     def __str__(self):
--- archive/util.py
+++ archive/util.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+from .models import Archive, Resource
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,12 +8,54 @@ options = Options()
 options.headless = True
 
 
-def get_screenshot(url=None, folder="uploads/", filename="screenshot.png"):
+def get_screenshot(url=None, path=None, folder="uploads/", filename="screenshot.png"):
     """
     Gets a screenshot using selenium
     """
-    path = f"{folder}/{filename}"
+    if path is None:
+        path = f"{folder}/{filename}"
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(url)
     driver.save_screenshot(path)
     driver.quit()
+
+
+def get_event_id(instance):
+    """
+    Generates a unique identifier for naming the screenshot
+    """
+    event_id = instance.created_at.strftime("%Y-%m-%d-%H-%M-%S-") + str(uuid4())
+    return event_id
+
+
+def path_generator(instance):
+    """
+    Uses event_id and resource_id to generate a unique path
+    """
+    return f"uploads/archive/{get_event_id(instance)}.png"
+
+
+def populate_archive(instance):
+    """
+    Populates the archive image fields
+    """
+    url = instance.resource.url
+    path = path_generator(instance)
+    instance.status = "failed"
+    instance.save()
+    get_screenshot(url, path)
+    instance.source = path
+    instance.status = "complete"
+    instance.save()
+
+
+def archive(url):
+    """
+    Prepares an archive model for the given resource.
+    """
+    resource = Resource.objects.get_or_create(url=url)[0]
+    archive = Archive.objects.create(resource=resource)
+    # TODO: Enqueue the archive to be populated
+    archive.status = "enqueued"
+    archive.save()
+    populate_archive(archive)
